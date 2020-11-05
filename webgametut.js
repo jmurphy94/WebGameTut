434f351b94a87f8c4fd608b82d681081dda93fb3
jmurphy94 <jeffmurphy94@gmail.com>
2020-11-05 17:16:41 -0600

    completed stage 3

--- webgametut.js
+++ webgametut.js
@@ -7,7 +7,6 @@ var gameCanvas = {
         this.canvas.width = 600;
         this.canvas.height = 400;
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
     },
     clear : function() {
@@ -19,7 +18,7 @@ var gamePiece;
 
 function startGame () {
     gameCanvas.start();
-    gamePiece = new component(30, 30, "red", 10, 370);
+    gamePiece = new component(30, 30, "red", 10, 170);
 }
 
 function component(width, height, color, x, y) {
@@ -28,13 +27,14 @@ function component(width, height, color, x, y) {
     this.x = x;
     this.y = y;
     this.update = function() {
-        ctx = gameArea.context;
+        ctx = gameCanvas.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
 function updateGameArea() {
-    gameArea.clear();
+    gameCanvas.clear();
+    gamePiece.x += 1;
     gamePiece.update();
 }
