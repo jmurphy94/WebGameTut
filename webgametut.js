6fa43fd5a4b6a6a4dbaa69550bf9cc887f3747a6
jmurphy94 <jeffmurphy94@gmail.com>
2020-11-05 21:21:11 -0600

    stage 4, multi keyboard key inputs

--- webgametut.js
+++ webgametut.js
@@ -6,8 +6,15 @@ var gameCanvas = {
     start : function () {
         this.canvas.width = 600;
         this.canvas.height = 400;
+        this.keys = [];
         this.context = this.canvas.getContext("2d");
-        this.interval = setInterval(updateGameArea, 20);
+        this.interval = setInterval(updateGameCanvas, 50);
+        window.addEventListener('keydown', function (e) {
+            gameCanvas.keys[e.key] = true;
+        })
+        window.addEventListener('keyup', function (e) {
+            gameCanvas.keys[e.key] = false;
+        })
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -18,12 +25,14 @@ var gamePiece;
 
 function startGame () {
     gameCanvas.start();
-    gamePiece = new component(30, 30, "red", 10, 170);
+    gamePiece = new component(30, 30, "red", 285, 185);
 }
 
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
     this.x = x;
     this.y = y;
     this.update = function() {
@@ -31,10 +40,44 @@ function component(width, height, color, x, y) {
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
 }
 
-function updateGameArea() {
+function updateGameCanvas() {
     gameCanvas.clear();
-    gamePiece.x += 1;
+    moveStop();
+    handleKeyPress();
+    gamePiece.newPos();
     gamePiece.update();
+}
+
+function handleKeyPress() {
+    if (gameCanvas.keys["ArrowUp"]) {moveUp();}
+    if (gameCanvas.keys["ArrowDown"]) {moveDown();}
+    if (gameCanvas.keys["ArrowLeft"]) {moveLeft();}
+    if (gameCanvas.keys["ArrowRight"]) {moveRight();}
+} 
+
+function moveUp() {
+    gamePiece.speedY -= 1;
+}
+
+function moveDown() {
+    gamePiece.speedY += 1;
+}
+
+function moveLeft() {
+    gamePiece.speedX -= 1;
+}
+
+function moveRight() {
+    gamePiece.speedX += 1;
+}
+
+function moveStop() {
+    gamePiece.speedX = 0;
+    gamePiece.speedY = 0;
 }
